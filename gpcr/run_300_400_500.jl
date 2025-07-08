a754c7b46dc5a1c542b7adde5d72259bfa8d08cd
Holly Huber <45047313+hhuber2512@users.noreply.github.com>
2025-07-08 00:57:40 -0700

    initial commit

--- gpcr/run_300_400_500.jl
+++ gpcr/run_300_400_500.jl
@@ -5,6 +5,3 @@ run = 200
 @nbinclude("300_process_posterior_samples.ipynb")
 @nbinclude("400_posterior_convergence_diagnostics.ipynb")
 @nbinclude("500_generate_predictions.ipynb")
-
-run = 201
-@nbinclude("300_process_posterior_samp
